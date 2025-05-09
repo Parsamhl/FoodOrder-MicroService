7b36011359a9d17d2ed8530527838bf31fb6e06f
parsa mousavi <mparsah4@gmail.com>
2025-05-10 00:18:21 +0330

    UserRepository Class Created

---
+++ src/Services/AccountMangement/02-Infrastructure/DataAccess/App.Infra.DataAcess/User/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using App.Domain.Core.User.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Infra.DataAcess.User.Repositories
+{
+    internal class UserRepository : IUserRepository
+    {
+        public Task AddAsync(Domain.Core.User.Entities.User user, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Domain.Core.User.Entities.User>> GetAllAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Domain.Core.User.Entities.User> GetByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
