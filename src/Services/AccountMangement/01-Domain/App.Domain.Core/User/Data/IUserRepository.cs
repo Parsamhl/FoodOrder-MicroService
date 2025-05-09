1e5c12298fdf3cadaa6a6e4ef0900fa2b183004e
parsa mousavi <mparsah4@gmail.com>
2025-05-10 00:11:53 +0330

    Application Layer added Core Updated in Accounting Service

---
+++ src/Services/AccountMangement/01-Domain/App.Domain.Core/User/Data/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿
+namespace App.Domain.Core.User.Data
+{
+    public interface IUserRepository
+    {
+        Task AddAsync(Entities.User user , CancellationToken cancellationToken);
+        Task<List<Entities.User>> GetAllAsync(CancellationToken cancellationToken);    
+        Task<Entities.User> GetByIdAsync(int id , CancellationToken cancellationToken);
+
+    }
+}
